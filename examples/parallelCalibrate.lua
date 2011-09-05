1e5c46ffcd7aaed17d325ff969d2440c5b90ec97
Clement Farabet <clement.farabet@gmail.com>
2011-09-05 17:15:58 -0400

    added examples.

---
+++ examples/parallelCalibrate.lua
@@ -0,0 +1,34 @@
+
+-- required libs
+require 'parallel'
+require 'lab'
+
+print = parallel.print
+
+-- parent code:
+function parent()
+   -- declare machines to use
+   parallel.addremote({ip='localhost', cores=4, lua='~/lua-local/bin/lua'},
+                      {ip='localhost', cores=4, lua='~/lua-local/bin/lua'})
+
+   -- run calibration
+   parallel.calibrate()
+
+   -- print coefs obtained
+   for _,remote in ipairs(parallel.remotes) do
+      print(remote.ip .. ' has a speed coef of ' .. remote.speed)
+   end
+
+   -- creating 8 children
+   print('free cores: ' .. parallel.remotes.cores)
+   forked = parallel.sfork(parallel.remotes.cores)
+   print('created ' .. #forked .. ' children, with speed coefs:')
+   for _,forked in ipairs(forked) do
+      print('id: ' .. forked.id .. ', speed = ' .. forked.speed)
+   end
+end
+
+-- protected execution:
+ok,err = pcall(parent)
+if not ok then print(err) end
+parallel.close()
---
+++ examples/parallelForkAndSync.lua
@@ -0,0 +1,53 @@
+
+-- required libs
+require 'parallel'
+require 'lab'
+
+-- calibration code:
+calib = [[
+      require 'torch'
+      require 'sys'
+      s = torch.Tensor(100000):fill(1)
+      d = torch.Tensor(100000):fill(0)
+      parallel.yield()
+      sys.tic()
+      for i = 1,10000 do
+         d:add(13,s)
+      end
+      time = sys.toc()
+      parallel.parent:send(time)
+]]
+
+-- parent code:
+function parent()
+   for i = 1,2 do
+      -- for 4 processes
+      parallel.sfork(4)
+
+      -- verify creation
+      parallel.print('currently have ' .. parallel.nchildren .. ' children')
+
+      -- exec code
+      parallel.children:exec(calib)
+
+      -- trigger computations
+      parallel.children:join()
+
+      -- receive results
+      times = parallel.children:receive()
+      for i,time in pairs(times) do
+         print('time taken by process ' .. i .. ': ' .. time)
+      end
+
+      -- sync processes (wait for them to die)
+      parallel.children:sync()
+
+      -- end status
+      parallel.print('currently have ' .. parallel.nchildren .. ' children')
+   end
+end
+
+-- protected execution:
+ok,err = pcall(parent)
+if not ok then print(err) end
+parallel.close()
